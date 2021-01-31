0de9242495394318ac0f54acc2b4ba0d77fde7ea
Michelle Kuchera <michelle.kuchera@gmail.com>
2021-01-31 11:45:30 -0500

    problem 2.5

---
+++ Homework1/quantum_step.py
@@ -0,0 +1,43 @@
+"""
+
+name: altitude.py
+
+This program calculates transmission and reflection probabilities from a
+particle incident on a quantum potential step.
+
+Problem 2.5 from Newman's Computational Physics.
+
+author: Jessica Jones
+date created: 1/11/2018
+date edited: 1/31/2021
+
+"""
+#imports
+
+#user-defined functions
+
+def probabilities():
+    """
+    
+
+    Returns
+    -------
+    transmission : transmission probability
+    reflection : reflection probability
+
+    """
+    # complete me
+
+    return transmission, reflection
+
+# main function
+def main():
+
+    T, R = probabilities()
+
+    # complete me: print results in a readable way and check that the 
+    # total probability is 1
+
+#run this code
+if __name__ == "__main__":
+    main()
