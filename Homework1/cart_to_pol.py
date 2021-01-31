c4a0a5b4b9e93c97c34bbd9c1ed9b7977fe4bae7
Michelle Kuchera <michelle.kuchera@gmail.com>
2021-01-31 11:42:08 -0500

    problem 2.3

---
+++ Homework1/cart_to_pol.py
@@ -0,0 +1,51 @@
+"""
+name: cart_to_poly.py
+
+This program converts from cartesian to polar coordinates.
+Problem 2.3 from Newman's Computational Physics.
+
+
+author: Jessica Jones
+date created: 1/11/2018
+date edited: 1/31/2021
+
+"""
+
+#imports
+
+
+
+# user defined functions
+def return_polar(x,y):
+    """
+
+    Parameters
+    ----------
+    x : x coordinate
+    y : y coordinate
+
+    Returns
+    -------
+    r : radius
+    theta_deg : angle in degrees
+
+    """
+    # complete me
+
+
+    return r, theta_deg
+
+
+def main():
+    #complete me: ask for user input
+    
+    
+    
+    r, theta = return_polar(x,y) # get the r, theta for a given x,y
+    
+    #complete me: print results in a readable manner
+
+
+# run main function
+if __name__ == "__main__":
+    main()
