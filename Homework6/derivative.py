5a377bad4c08c64b4dfdad93c5b4892014ceda2f
Michelle Kuchera <michelle.kuchera@gmail.com>
2021-03-22 20:32:04 -0400

    Homework 6

---
+++ Homework6/derivative.py
@@ -0,0 +1,80 @@
+"""
+
+name: derivative.py
+
+Compute and visualize numerical and analytic derivatives.
+
+Problem 5.15 from Newman's Computational Physics.
+
+author: Shuri
+date created: 22/3/2021
+
+
+"""
+# Reminder: you can create other helper functions as long as the function
+# behaviors below remain the same
+
+
+
+# function name: analytic
+
+    """
+    Compute analytic derivative of f(x): df/dx
+
+    Parameters
+    ----------
+    x : array of FLOAT
+        x values to compute the derivative at.
+
+    Returns
+    -------
+    df : array of FLOAT
+        derivative values at associated x values
+
+    """
+
+#function name: central
+
+    """
+    Take derivative of f w.r.t x using central differences.
+
+    Parameters
+    ----------
+    x : array of FLOAT
+        x values to compute the derivative at.
+
+    Returns
+    -------
+    df : array of FLOAT
+        derivative values at associated x values
+
+    """
+
+# function name: make_plot
+
+    """
+    
+
+    Parameters
+    ----------
+    x : array of FLOAT
+        DESCRIPTION.
+    y_analytic : array of FLOAT
+        DESCRIPTION.
+    y_numeric : array of FLOAT
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+
+
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
---
+++ Homework6/gravity.py
@@ -0,0 +1,57 @@
+"""
+
+name: gravity.py
+
+Model the gravitational pull of a galaxy.
+
+Problem 5.14 from Newman's Computational Physics.
+
+author: Shuri
+date created: 22/3/2021
+
+
+"""
+
+
+
+# function name: force
+
+    """
+    Calculate force as a function of distance above the sheet, z.
+
+    Parameters
+    ----------
+    z : array of FLOAT
+        z values to calculate force for.
+
+    Returns
+    -------
+    F : array of FLOAT
+        Force at each z value.
+
+    """
+
+# function name: make_plot
+
+    """
+    Visualize force as a function of distance above the sheet, z.
+
+    Parameters
+    ----------
+    z : array of FLOAT
+        z values to visualize force for.
+
+    Returns
+    -------
+    None.
+
+    """
+
+
+
+def main():
+
+
+
+if __name__ == "__main__":
+    main()
